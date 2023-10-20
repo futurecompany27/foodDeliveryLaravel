9a1911188150dce051927a52b749c653cf58be71
Ravindra <ravindra@arkatechs.com>
2023-10-20 15:10:08 +0530

    test

--- resources/views/ChefEmailVerification.blade.php
+++ resources/views/ChefEmailVerification.blade.php
@@ -1,23 +1,20 @@
-<!DOCTYPE html>
-<html>
+<!DOCTYPE html> <html>
 
-<head>
-    <title>Home Shef Registration </title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-</head>
+<head> <title>Home Shef Registration </title> <link rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    </head>
 
-<body>
+    <body>
 
-    <b>Hi {{ucfirst($full_name)}}</b>
-    <p>Your account has been created successfully.</p>
-    <p>You may access your account only after the verification of your documents and details.</p>
+        <b>Hi {{ucfirst($full_name)}}</b>
+        <p>Your account has been created successfully.</p>
+        <p>You may access your account only after the verification of your documents and details.</p>
 
-    <p>Kindly verify your email by clicking on the link below</p>
-    <a class="btn btn-primary" href="{{ env('domain'). 'verified-mail?id=' . $id . '&type=chef' }}">Email Verification</a>
-    <p>Regards,</p>
-    <img src="{{'/image/mainlogo.png'}}" class="object-fit-cover img-fluid" width="80" alt="">
-    <p>Homeplate Team</p>
-    <p>support@homeplate.ca</p>
-</body>
+        <p>Kindly verify your email by clicking on the link below</p>
+        <a class="btn btn-primary" href="{{ env('domain'). 'verified-mail?id=' . $id . '&type=chef' }}">Email
+            Verification</a>
+        <p>Regards,</p>
+        
+    </body>
 
-</html>
+    </html>
--- resources/views/UserEmailVerification.blade.php
+++ resources/views/UserEmailVerification.blade.php
@@ -1,20 +1,26 @@
 <!DOCTYPE html>
 <html>
+
 <head>
- <title>HomeShef Registration </title>
- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <title>HomeShef Registration </title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
-<body>
- 
- <b>Hi {{ucfirst($fullname)}}</b>
- <p>Your account has been created successfully.</p>
- <p>You may access your account only after the verification of your documents and details.</p> 
 
- <p>Kindly verify your email by clicking on the link below</p>
+<body> <b>Hi {{ucfirst($fullname)}}</b>
+    <p>Your account has been created successfully.</p>
+    <p>You may access your
+        account only after the verification of your documents and details.</p>
+    <p>Kindly verify your email by clicking on
+        the link below</p> <a class="btn btn-primary"
+        href="{{ env('domain') . 'verified-mail?id=' . $id . '&type=user' }}">
+        Email
+        Verification<a>
 
- <a class="btn btn-primary" href="{{ env('domain') . 'verified-mail?id=' . $id . '&type=user' }}">Email Verification<a>
-
- <p>Regards,</p>
- <p>Homeshef Team</p>
+            <p>Regards,</p>
+            <img src="{{env('filePath') . 'storage/admin/logos/mainlogo.png'}}" class="object-fit-cover img-fluid"
+                width="80" alt="">
+            <p>Homeplate Team</p>
+            <p>support@homeplate.ca</p>
 </body>
-</html> 
+
+</html>
